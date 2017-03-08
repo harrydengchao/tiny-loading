056f7c1ec1176711c5a5db9c0f1c38d76c718047
harry <dengchao2056@126.com>
2017-03-08 15:44:39 +0800

    first

---
+++ src/loading.js
@@ -0,0 +1,29 @@
+import { Indicator } from 'mint-ui';
+
+class Load {
+  constructor() {
+    this.config = {
+      text: '加载中...'
+    }
+  }
+
+  open(args) {
+    // 参数为空时
+    if (!args) {}
+    else if (args.constructor === String) {
+      this.config.text = args;
+    }
+    else if (args.constructor === Object) {
+      Object.assign(this.config, args)
+    }
+    Indicator.open(this.config)
+  }
+
+  close() {
+    Indicator.close()
+  }
+}
+
+const Loading = new Load();
+
+export default Loading
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    dist: './src/loading.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: '[name].min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        query: {
+          name: 'fonts/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        query: {
+          name: 'images/[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  // module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}

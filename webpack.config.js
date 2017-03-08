2a0a6095b341af3646c08f3fc06d9e34222db01c
harry <dengchao2056@126.com>
2017-03-08 18:05:09 +0800

    xx

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,10 @@ var path = require('path')
 var webpack = require('webpack')
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const extractSass = new ExtractTextPlugin({
-    filename: "[name].[contenthash].css",
-    disable: process.env.NODE_ENV === "development"
-});
-
 module.exports = {
   entry: {
     dist: './src/loading.js',
+    vendor: ['vue'],
   },
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -57,7 +53,6 @@ if (process.env.NODE_ENV === 'production') {
   // module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
-    extractSass,
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
@@ -71,6 +66,26 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
+    }),
+    // split vendor js into its own file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(
+            path.join(__dirname, '../node_modules')
+          ) === 0
+        )
+      }
+    }),
+    // extract webpack runtime and module manifest to its own file in order to
+    // prevent vendor hash from being updated whenever app bundle is updated
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      chunks: ['vendor'],
     })
   ])
 }
